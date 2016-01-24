b012fe21cee08c07aa8fadb6786163582eafd5ac
PavelCreator <pavelcreator@gmail.com>
2016-01-24 20:30:15 +0200

    focusToField to body

--- src/start.js
+++ src/start.js
@@ -4,3 +4,11 @@ clipboard.cancelManualEntry();
 reset();
 focusToField();
 
+document.getElementsByTagName('body')[0].onmousedown = function () {
+  document.getElementById('field').style.padding = "10px 9px 8px";
+}
+
+document.getElementsByTagName('body')[0].onclick = function () {
+  document.getElementById('field').style.padding = "10px 8px 8px";
+  focusToField();
+}
